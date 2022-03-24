3435f5e4e8175291d2f3757a49939e6c400ea527
Mukal Dadhwal <mukaldadhdwal@gmail.com>
2022-03-24 10:24:44 +0530

    added generatePaySlip, modified updateSalary to decrement, input from user to enter username and password. Project Complete!!

--- code.py
+++ code.py
@@ -1,48 +1,23 @@
-# CODE FOR PROJECT
-'''
-1. adding employee
-2. Deleting employee
-3. retrieving
-4. modifying
-5. generate pay slip
-6. exit
-
-EMPLOYEE Record->
-Employee id
-NAME
-GENDER
-DEPARTMENT
-SALARY
-LOCATION
-ACCOUNT NO(7digit)
-
-PROPERITES ->
-EMPLOYEE ID: int
-NAME: varchar
-DEPARTMENT: FINANCE/HR/ACCOUNTS/ADMIN/SECURITY/OPERATIONS
-SALARY: int
-GENDER: M/F
-
-'''
-
-from dataclasses import FrozenInstanceError
 from mysql.connector import MySQLConnection, Error
 import mysql.connector as connector
 import time
 
+username = input('Enter username of mysql: ')
+password = input('Enter password of mysql: ')
+
 # CHECKING CONNECTION
 try:
-    db = connector.connect(host='localhost', user='root', passwd='ROOT', database='employee_management')
+    db = connector.connect(host='localhost', user=username, passwd=password, database='employee_management')
     db.close()
 except:
-    db = connector.connect(host="localhost", user="root", passwd="ROOT")
+    db = connector.connect(host="localhost", user=username, passwd=password)
     cursor = db.cursor()
 
     createDatabaseQuery = "create database employee_management"
     createTableQuery = "create table employee(emp_id int(5) NOT NULL PRIMARY KEY, emp_name varchar(30) NOT NULL , gender char(1) NOT NULL, department varchar(11) NOT NULL, salary int NOT NULL, location varchar(20) NOT NULL, Accountno int(7) NOT NULL)"
 
     cursor.execute(createDatabaseQuery)
-    db = connector.connect(host="localhost", user="root", passwd="ROOT", database='employee_management')
+    db = connector.connect(host="localhost", user=username, passwd=password, database='employee_management')
 
     cursor = db.cursor()
     cursor.execute(createTableQuery)
@@ -50,24 +25,24 @@ except:
     cursor.close()
     db.close()
 
-mydb = connector.connect(host='localhost', user='root', passwd='ROOT', database='employee_management')
+mydb = connector.connect(host='localhost', user=username, passwd=password, database='employee_management')
 mycursor = mydb.cursor()
 
 def mainLoop():
-    print("\t\t\t", time.ctime())
-    print("EMPLOYEES MANAGEMENT SYSTEM")
+    print(f'\t\t\t {time.ctime()}')
+    print("\t\t\t EMPLOYEES MANAGEMENT SYSTEM")
     response = "y"
 
     while response.lower() == 'y':
-        print("operations...")
-        print("1.employee registeration")
-        print("2.employee details")
-        print("3.update salary")
-        print("4.employees list")
-        print("5.remove employee")
-        print("6.generate pay slip")
-        print("7.know your salary")
-        print("8.Exiting")
+        print('\n')
+        print("\t\t\t Operations")
+        print("\t\t\t 1.employee registeration")
+        print("\t\t\t 2.employee details")
+        print("\t\t\t 3.update salary")
+        print("\t\t\t 4.employees list")
+        print("\t\t\t 5.remove employee")
+        print("\t\t\t 6.generate pay slip")
+        print("\t\t\t 7.Exiting")
 
         operation = int(input('Enter the operation to perform(number): '))
 
@@ -84,10 +59,9 @@ def mainLoop():
         elif operation == 6:
             generatePayslip()
         elif operation == 7:
-            pass
-            # get_salary()
-        elif operation == 7:
-            # print('Exiting...')
+            mycursor.close()
+            mydb.close()
+            print('Thanks! Have a nice day.')
             break
         else:
             print("Sorry Couldn't find that one ...")
@@ -153,8 +127,8 @@ def getDetails():
     except Error as _:
         print('Something went wrong there!')
         print('Please try again...')
-    employeeTuple = mycursor.fetchall()[0] # LIST(<TUPLE>)
-    print('Employee details ...')
+    employeeTuple = mycursor.fetchall()[0] 
+    print(f'Employee details {employeeTuple[1]}...')
     
     print('\n')
 
@@ -164,19 +138,34 @@ def getDetails():
 
 def updateSalary():
     employeeId = input('Enter the employee id: ')
-    increment = input('Enter the amount to increment: ')
 
-    print('\n')
+    toIncrementOrDecrement = int(input('Enter operation (increment -> 1 decrement -> 2): '))
 
-    try:
-        query = "UPDATE employee SET salary = salary + '%s' WHERE emp_id = '%s'" % (increment, employeeId)
-        mycursor.execute(query)
-        mydb.commit()
-        print('Salary updated successfully!')
-    except Error as _:
-        print('Something went wrong there!')
-        print('Please try again...')
-        mydb.rollback()
+    if toIncrementOrDecrement == 1:
+        increment = input('Enter the amount to increment: ')
+
+        try:
+            query = "UPDATE employee SET salary = salary + '%s' WHERE emp_id = '%s'" % (increment, employeeId)
+            mycursor.execute(query)
+            mydb.commit()
+            print('Salary updated successfully!')
+        except Error as _:
+            print('Something went wrong there!')
+            print('Please try again...')
+            mydb.rollback()
+
+    else:
+        decrement = input('Enter the amount to decrement: ')
+
+        try:
+            query = "UPDATE employee SET salary = salary - '%s' WHERE emp_id = '%s'" % (decrement, employeeId)
+            mycursor.execute(query)
+            mydb.commit()
+            print('Salary updated successfully!')
+        except Error as _:
+            print('Something went wrong there!')
+            print('Please try again...')
+            mydb.rollback()
 
 def getEmployeeList():
     print('EMPLOYEE LIST...')
@@ -186,7 +175,7 @@ def getEmployeeList():
     try:
         query = "SELECT * FROM employee"
         mycursor.execute(query)
-        employeeList = mycursor.fetchall() # List(<tuple>)
+        employeeList = mycursor.fetchall()
         for row in employeeList:
             print('%11s'%str(row[0]), '%17s'%row[1], '%9s'%row[2], '%15s'%row[3], '%9s'%str(row[4]), '%13s'%row[5], '%11s'%str(row[6]))
     except Error as _:
@@ -205,6 +194,76 @@ def deleteEmployee():
         print('Something went wrong there!')
         print('Please try again...')
         mydb.rollback()
+
 def generatePayslip():
-    pass
+    employeeId = input('Enter the employee id: ')
+    print('\n')
+
+    data = tuple()
+
+    try:
+        query = "SELECT * FROM employee WHERE emp_id='%s'" % employeeId
+        mycursor.execute(query)
+        listOfEmployees = mycursor.fetchall()
+
+        for emp in listOfEmployees:
+            if str(emp[0]) == employeeId:
+                data = emp
+  
+        mydb.commit()
+    except Error as _:
+        print('Something went wrong there!')
+        print('Please try again...')
+        mydb.rollback()
+
+    employeeName = data[1]
+    employeeSalary = data[4]
+    departmentName = data[3]
+    city = data[5]
+    accountNumber = data[6]
+
+    medicalAllowance = 0.02 * employeeSalary
+    dailyAllowance = 0.01 * employeeSalary
+    houseRentAllowance = 0.05 * employeeSalary
+    gross = employeeSalary + medicalAllowance + dailyAllowance + houseRentAllowance
+    pf = 0.02 * employeeSalary
+    it = 0.04 * employeeSalary
+    deduction = pf + it
+
+    net = gross - deduction
+
+    print(f'\t\t\t ---------------SALARY SLIP OF {employeeName}----------------')
+
+    print(f'\t\t\t Employee ID:', employeeId)
+    print(f'\t\t\t Name of the employee:', employeeName)
+    print(f'\t\t\t Department of employee:', departmentName)
+    print(f'\t\t\t City of employee:', city)
+
+    print(f'\t\t\t Account number:', accountNumber)
+
+    print('\t\t\t Basic Salary:', employeeSalary)
+    print('\n')
+
+    print('\t\t\t Medical Allowance:\t\t', medicalAllowance)
+
+    print('\t\t\t Daily Allowance:\t\t', dailyAllowance)
+
+    print('\t\t\t House Rent Allowed:\t\t', houseRentAllowance)
+
+    print('\t\t\t +')
+    print('\t\t\t -----------------------------------------------')
+    print('\t\t\t Gross Salary\t\t', gross)
+
+    print('\n')
+    print('\t\t\t Provident Fund\t\t', pf)
+
+    print('\t\t\t Income Tax\t\t', it)
+
+    print('\t\t\t +')
+    print('\t\t\t -----------------------------------------------')
+
+    print('\t\t\t Deductions:\t\t', deduction)
+
+    print('\t\t\t Net Salary = Gross - Deductions\t\t₹', net)
+
 mainLoop()
